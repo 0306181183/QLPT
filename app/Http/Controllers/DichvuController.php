73375fc4134274971b5afb855a44375805e862aa
0306181183 <0306181183@caothang.edu.vn>
2021-04-10 09:31:55 +0700

    rep

--- app/Http/Controllers/DichvuController.php
+++ app/Http/Controllers/DichvuController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\PREs\PRE_Dichvu;
 use Messages as MES;
 use App\REPs\REP_Dichvu;
-use App\VALs\VAL_Dichvu;
+use App\VALs\VAL_Giadichvu;
 use Illuminate\Http\Request;
 use PHPUnit\Util\Json;
 
@@ -13,10 +13,10 @@ class DichvuController extends Controller
 {
 
 
-    private VAL_Dichvu $val;
+    private VAL_Giadichvu $val;
     private REP_Dichvu $rep;
     private PRE_Dichvu $pre;
-    public function __construct(VAL_Dichvu $val, REP_Dichvu $rep, PRE_Dichvu $pre)
+    public function __construct(VAL_GiaDichvu $val, REP_Dichvu $rep, PRE_Dichvu $pre)
     {
         $this->val = $val;
         $this->rep = $rep;
